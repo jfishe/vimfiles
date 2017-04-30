7002121c1dd5f31ecbc4cf8618c8d3dab892d1ef
jfishe <jdfenw@gmail.com>
2017-04-30 16:21:30 -0700

    Updated PYTHONPATH & Anaconda env management
    * Added vim-conda alternative to changing PYTHONPATH
    * Updated vimsub.sh for several new submodules

--- vimfiles/bundle/vimsub.sh
+++ vimfiles/bundle/vimsub.sh
@@ -9,7 +9,10 @@ for a in \
 "https://github.com/vim-pandoc/vim-pandoc-syntax.git" \
 "https://github.com/altercation/vim-colors-solarized.git" \
 "https://github.com/tpope/vim-surround.git" \
-"https://github.com/vimwiki/vimwiki.git"
+"https://github.com/vimwiki/vimwiki.git" \
+"https://github.com/w0rp/ale.git" \
+"https://github.com/tpope/vim-sensible.git" \
+"https://github.com/jfishe/vim-conda.git"
 do
     git submodule add ${a}
 done
