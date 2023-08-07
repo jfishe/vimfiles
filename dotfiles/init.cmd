6ec77ce3b2464e76f8bc452eafd931e2c2b15dfa
John D. Fisher <jdfisher@energy-northwest.com>
2023-08-07 15:59:35 -0700

    fix(dotfiles): quote init.cmd correctly

--- dotfiles/init.cmd
+++ dotfiles/init.cmd
@@ -7,9 +7,9 @@ REM reg delete "HKCU\Software\Microsoft\Command Processor" /v AutoRun
 :: if exist "%USERPROFILE%\Miniconda3\condabin\conda_hook.bat" call "%USERPROFILE%\Miniconda3\condabin\conda_hook.bat"
 
 setlocal
-set CONDA_BAT="Miniconda3\Scripts\activate.bat"
-if exist "%LOCALAPPDATA%\%CONDA_BAT%" set COND_BAT="%LOCALAPPDATA%\%CONDA_BAT%"
-if exist "%USERPROFILE%\%CONDA_BAT%" set COND_BAT="%USERPROFILE%\%CONDA_BAT%"
+set CONDA_BAT=Miniconda3\Scripts\activate.bat
+if exist "%LOCALAPPDATA%\%CONDA_BAT%" set COND_BAT=%LOCALAPPDATA%\%CONDA_BAT%
+if exist "%USERPROFILE%\%CONDA_BAT%" set COND_BAT=%USERPROFILE%\%CONDA_BAT%
 if not exist "%COND_BAT%" goto :eof
 
-call "%COND_BAT" vim-python
+call "%COND_BAT%" vim-python
