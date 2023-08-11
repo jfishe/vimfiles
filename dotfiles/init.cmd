0697cbb5675c843d075c999feec86c382cb6b691
John D. Fisher <jdfenw@gmail.com>
2023-08-11 14:49:25 -0700

    fix(conda): remove setlocal
    Windows 11 keeps setlocal when other batch scripts called, unlike
    Windows 10.

--- dotfiles/init.cmd
+++ dotfiles/init.cmd
@@ -4,12 +4,9 @@ REM reg add "HKCU\Software\Microsoft\Command Processor" /v AutoRun ^
 REM   /t REG_EXPAND_SZ /d "%"USERPROFILE"%\.init.cmd" /f
 REM To remove AutoRun:
 REM reg delete "HKCU\Software\Microsoft\Command Processor" /v AutoRun
-:: if exist "%USERPROFILE%\Miniconda3\condabin\conda_hook.bat" call "%USERPROFILE%\Miniconda3\condabin\conda_hook.bat"
 
-setlocal
-set CONDA_BAT=Miniconda3\Scripts\activate.bat
-if exist "%LOCALAPPDATA%\%CONDA_BAT%" set COND_BAT=%LOCALAPPDATA%\%CONDA_BAT%
-if exist "%USERPROFILE%\%CONDA_BAT%" set COND_BAT=%USERPROFILE%\%CONDA_BAT%
-if not exist "%COND_BAT%" goto :eof
+@IF DEFINED CONDA_SHLVL GOTO :EOF
 
-call "%COND_BAT%" vim-python
+@if exist "%USERPROFILE%\Miniconda3\condabin\conda_hook.bat" call "%USERPROFILE%\Miniconda3\condabin\conda_hook.bat"
+@if exist "%LOCALAPPDATA%\Miniconda3\condabin\conda_hook.bat" call "%LOCALAPPDATA%\Miniconda3\condabin\conda_hook.bat"
+@call "%CONDA_BAT%" activate vim-python
